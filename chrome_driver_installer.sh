14d73863e64a50fce93c4ee92f5f1a330052fdbc
sagar-unix <54767757+sagar-unix@users.noreply.github.com>
2021-05-16 03:45:39 +0530

    Update chrome_driver_installer.sh

--- chrome_driver_installer.sh
+++ chrome_driver_installer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+cd /tmp/
 sudo curl https://intoli.com/install-google-chrome.sh | bash
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
 google-chrome –-version && which google-chrome
-cd /tmp/
 chrome_version=$(yum info  google-chrome|grep -e "Version\b.*[d\.]*"|cut -d ':' -f2|cut -d "." -f1,2,3|awk '{$1=$1};1')
 exact_chrome_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chrome_version})
 rm chromedriver_linux64.zip
