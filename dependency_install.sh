a9b0c84a595583ad429196ab303e112b6e88e8ac
sagar-unix <54767757+sagar-unix@users.noreply.github.com>
2021-05-16 19:12:06 +0530

    Update dependency_install.sh

--- dependency_install.sh
+++ dependency_install.sh
@@ -4,10 +4,10 @@
 #Designation: DevOps Engineer
 set -x 
 cd /tmp/
-yum erase -y google-chrome-stable || true
+yum -y erase google-chrome-stable || true
 rm -rf /opt/google/chrome || true
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-yum localinstall google-chrome-stable_current_x86_64.rpm
+yum -y localinstall google-chrome-stable_current_x86_64.rpm
 rm google-chrome-stable_current_x86_64.rpm
 chrome_version=$(yum info  google-chrome-stable |grep -e "Version\b.*[d\.]*"|cut -d ':' -f2|cut -d "." -f1,2,3|awk '{$1=$1};1')
 exact_chrome_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chrome_version})
