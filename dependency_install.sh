ddae2daef2842c6948ee0020c1db9e42750fba06
sagar-unix <54767757+sagar-unix@users.noreply.github.com>
2021-05-16 18:57:58 +0530

    Update dependency_install.sh

--- dependency_install.sh
+++ dependency_install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+#Author: SAGAR
+#ID : sagar-unix
+#Designation: DevOps Engineer
 set -x 
 cd /tmp/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-yum localinstall -y google-chrome-stable_current_x86_64.rpm
+yum localinstall google-chrome-stable_current_x86_64.rpm
 rm google-chrome-stable_current_x86_64.rpm
-google-chrome –-version && which google-chrome
 chrome_version=$(yum info  google-chrome-stable |grep -e "Version\b.*[d\.]*"|cut -d ':' -f2|cut -d "." -f1,2,3|awk '{$1=$1};1')
 exact_chrome_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chrome_version})
 rm chromedriver_linux64.zip
@@ -12,4 +14,4 @@ wget  https://chromedriver.storage.googleapis.com/${exact_chrome_version}/chrome
 unzip -o chromedriver_linux64.zip -d /opt/google/chrome/
 chmod a+x /opt/google/chrome/chromedriver
 ln -s /opt/google/chrome/chromedriver /usr/bin/chromedriver
-chromedriver – version
+exit 0
